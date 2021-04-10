b93bc232efde81071f46da75980f95342fc8d896
CajoAlbuquerque <carlosjo.da@gmail.com>
2021-04-10 16:09:50 +0100

    Python data parser; JS Torrents list with provider + infohash

---
+++ dataparsers/data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+opus_data = pd.read_csv('movie_summary.csv')
+opus_data
+
+relevant_info = opus_data[['movie_odid', 'display_name', 'production_budget', 'international_box_office', 'worldwide_release_date']]
+
+print(relevant_info)
+
+relevant_info.to_csv('movie_sum_filtered.csv', index_label=False, index=False, header=False)
--- infohashes/main.js
+++ infohashes/main.js
@@ -4,44 +4,49 @@ var fs = require('fs');
 
 // Get providers
 const providers = TorrentSearchApi.getProviders();
- 
+
 // Get active providers
 const activeProviders = TorrentSearchApi.getActiveProviders();
- 
+
 // Enable public providers
 TorrentSearchApi.enablePublicProviders();
 
-const getTorrents = (async ()=>{
-    return TorrentSearchApi.search('Endgame', 'Movies', 20)
+const getTorrents = (async () => {
+    return TorrentSearchApi.search('Titanic', 'Movies', 20)
         .then((torrents) => {
-            console.log(torrents[0])
-            return torrents[0]
+            return torrents;
         })
-        .then(async (torrent) => {
-            const magnetLink = torrent.magnet;
-            const peers = torrent.peers;
-            const seeds = torrent.seeds;
-            const torrentDetails = parseTorrent(magnetLink);
+        .then(async (torrentObj) => {
+            let objs = [];
 
-            var torrents = {
-                infohash: torrentDetails.infoHash,
-                name: torrentDetails.name,
-                trackers: torrentDetails.tr,
-                peers: peers,
-                seeds: seeds
-            };
+            for (const torrent of torrentObj) {
+                const magnetLink = await TorrentSearchApi.getMagnet(torrent);
 
-            // obj.table.push({id: 1, square:2});
+                const peers = torrent.peers;
+                const seeds = torrent.seeds;
+                const torrentDetails = parseTorrent(magnetLink);
 
-            var json = JSON.stringify(torrents);
+                var torrentObj = {
+                    infohash: torrentDetails.infoHash,
+                    name: torrentDetails.name,
+                    trackers: torrentDetails.tr,
+                    peers: peers,
+                    seeds: seeds,
+                    provider: torrent.provider
+                };
 
-            fs.writeFile('torrents.json', json, 'utf8', function (err, data) {
-                fs.writeFile('writeMe.txt', data, function(err, result) {
-                   if(err) console.log('error', err);
+                objs.push(torrentObj);
+            }
+
+            fs.writeFile('torrents.json', JSON.stringify(objs), 'utf8', function (err, data) {
+                if (!data) return;
+
+                fs.writeFile('writeMe.txt', data, function (err, result) {
+                    if (err) console.log('error', err);
                 });
             });
         })
-        .catch((error)=>{
+        .catch((error) => {
             console.log(error)
         })
 })
