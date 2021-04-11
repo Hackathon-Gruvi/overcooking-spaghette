368c9c053f5ed001b386d7149475a4cb4532d5c3
CajoAlbuquerque <carlosjo.da@gmail.com>
2021-04-11 12:40:03 +0100

    Datascience scripts

--- dataparsers/data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-opus_data = pd.read_csv('movie_summary.csv')
-
-relevant_info = opus_data[['movie_odid', 'display_name', 'genre', 'production_budget', 'international_box_office', 'worldwide_release_date']]
-
-print(relevant_info)
-
-relevant_info.to_csv('movie_sum_filtered.csv', index_label=False, index=False, header=False)
