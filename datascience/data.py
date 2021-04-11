f4f2b5ce1da993fd4efd11696400e01468c7e47f
CajoAlbuquerque <carlosjo.da@gmail.com>
2021-04-11 15:47:39 +0100

    Add results and data plots

--- datascience/data.py
+++ datascience/data.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+plt.close("all")
 
 # Movie summary info
 movie_summary_all = pd.read_csv('movie_summary.csv')
@@ -10,6 +14,9 @@ movie_ids = movie_ids[movie_ids['domain'] == 'IMDB']
 # Torrents
 torrent_data_all = pd.read_json('modifiedTPB.json')
 torrent_data = torrent_data_all.drop_duplicates(subset='imdb')
+# Fix torrent dates
+for ind, torrent in torrent_data.iterrows():
+    torrent_data.at[ind, 'time'] = datetime.strptime(torrent['time'], '%a, %d %b %Y %H:%M:%S %Z').strftime('%Y-%m-%d')
 # Aggregate seeders for same movies
 for ind1, torrent1 in torrent_data_all.iterrows():
     for ind2, torrent2 in torrent_data.iterrows():
@@ -24,7 +31,29 @@ torrent_data['lost_revenue'] = torrent_data['seeds'] * mean_ticket_price
 
 movie_data = pd.merge(movie_summary, movie_ids, on='movie_odid' , how='inner')
 crossed_data = pd.merge(movie_data, torrent_data, how='inner', left_on='id', right_on='imdb').sort_values('seeds', ascending=False)
+crossed_data['ratio'] = crossed_data['lost_revenue'] / crossed_data['total_box_office']
 
-# print(crossed_data[['title', 'genre', 'production_budget', 'total_box_office', 'lost_revenue', 'seeds', 'imdb']])
+temp_crossed = crossed_data[['time', 'worldwide_release_date']]
+for i, row in temp_crossed.iterrows():
+    hacked = datetime.strptime(row['time'], '%Y-%m-%d')
+    release = datetime.strptime(row['worldwide_release_date'], '%Y-%m-%d')
+    if release.year == hacked.year:
+        dif = (hacked - release).days
+        crossed_data.at[i, 'time_to_hack'] = dif
+    else:
+        crossed_data.at[i, 'time_to_hack'] = None
 
-print(crossed_data.genre.mode())
+crossed_data_prod_seeds = crossed_data[crossed_data['production_budget'] != 0]
+crossed_data_prod_seeds.plot.scatter(x='production_budget', y='seeds')
+plt.savefig("prod_seeds.png")
+
+crossed_data.plot.scatter(x='genre', y='seeds')
+plt.savefig("genre_seeds.png")
+
+
+crossed_data_time = (crossed_data[['title', 'worldwide_release_date', 'time', 'time_to_hack']]).dropna()
+print('Mean time to hack')
+print(crossed_data_time.time_to_hack.mean())
+crossed_data_time.plot.scatter(x='title', y='time_to_hack')
+plt.savefig("hack_time.png")
+
