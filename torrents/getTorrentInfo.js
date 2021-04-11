4ccbda964bc059c71f22d7941c62202f4164f83a
SALajes <sofialajes98@gmail.com>
2021-04-11 14:33:32 +0100

    Add infohash to movies to track and top100

--- torrents/getTorrentInfo.js
+++ torrents/getTorrentInfo.js
@@ -1,69 +1,11 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = 5001
 const TorrentSearchApi = require('torrent-search-api');
 const parseTorrent = require('parse-torrent');
 var fs = require('fs');
 
-// app.get('/', cors(), (req, res) => {
-//     const title = req.query.title;
-//     console.log("Request received; Title = " + title);
-//     getTorrentInfo(title).then((torrentsArray) => {
-//         res.send(torrentsArray);
-//     })
-// })
-
-// app.listen(port, () => {
-//     console.log(`Running at http://localhost:${port}`)
-// })
-
 // Enable public providers
 TorrentSearchApi.enablePublicProviders();
 
-const getTorrents = (async (title) => {
-    return TorrentSearchApi.search(title, 'Movies', 20)
-        .then((torrents) => {
-            return torrents;
-        })
-        .then(async (torrentObj) => {
-            let objs = [];
-
-            for (const torrent of torrentObj) {
-                const magnetLink = await TorrentSearchApi.getMagnet(torrent);
-
-                const peers = torrent.peers;
-                const seeds = torrent.seeds;
-                const torrentDetails = getTorrentInfo(magnetLink);
-                
-                var torrentObj = {
-                    infohash: torrentDetails.infoHash,
-                    name: torrentDetails.name,
-                    trackers: torrentDetails.tr,
-                    peers: peers,
-                    seeds: seeds,
-                    provider: torrent.provider
-                };
-
-                objs.push(torrentObj);
-            }
-
-            fs.writeFile('torrents.json', JSON.stringify(objs), 'utf8', function (err, data) {
-                if (!data) return;
-
-                fs.writeFile('writeMe.txt', data, function (err, result) {
-                    if (err) console.log('error', err);
-                });
-            });
-
-            return objs;
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-})
-
-const getTorrentInfo = (async (magnetLink) => {
+const getParseTorrentInfo = ((magnetLink) => {
     const torrentDetails = parseTorrent(magnetLink);
         
     var torrentInfo = {
@@ -74,3 +16,92 @@ const getTorrentInfo = (async (magnetLink) => {
 
     return torrentInfo
 })
+
+const getInfohashesForTop100 = (() => {
+    var top100 = JSON.parse(fs.readFileSync('topTPB.json'));
+
+    for(let movie of top100){
+        const magnetLink = movie.magnet
+        const torrentDetails = getParseTorrentInfo(magnetLink);
+
+        movie.infohash = torrentDetails.infohash
+        movie.name = torrentDetails.name
+        movie.trackers = torrentDetails.trackers
+    }
+    
+    fs.writeFile('top100wInfohash.json', JSON.stringify(top100), 'utf8', function (err, data) {
+        if (!data) return;
+
+        fs.writeFile('writeMe.txt', data, function (err, result) {
+            if (err) console.log('error', err);
+        });
+    });
+})
+
+const getTorrentsForMoviesToTrack = (async () => {
+    var moviesToTrack = JSON.parse(fs.readFileSync('moviesToTrack.json'));
+    const result = []
+
+    for(let movie of moviesToTrack){
+        const torrents = await getTorrentsByTitle(movie.title);
+        
+        if(torrents == null)
+            continue
+
+        for(let torrent of torrents){
+            movie.infohash = torrent.infohash
+            movie.name = torrent.name
+            movie.trackers = torrent.trackers
+            movie.peers = torrent.peers,
+            movie.seeds = torrent.seeds,
+            movie.provider = torrent.provider
+
+            result.push(movie)
+        }
+    }
+    
+    fs.writeFile('moviesToTrackTorrents.json', JSON.stringify(result), 'utf8', function (err, data) {
+        if (!data) return;
+
+        fs.writeFile('writeMe.txt', data, function (err, result) {
+            if (err) console.log('error', err);
+        });
+    });
+})
+
+const getTorrentsByTitle = (async (title) => {
+    const numberOfTorrentsByMovie = 1;
+    return TorrentSearchApi.search(title, 'Movies', numberOfTorrentsByMovie)
+        .then(async (torrents) => {
+            if(torrents.length == 0){
+                return null
+            }
+
+            let objs = [];
+
+            for (let torrent of torrents) {
+                let magnetLink = await TorrentSearchApi.getMagnet(torrent);
+                let torrentDetails = getParseTorrentInfo(magnetLink);
+                
+                let torrentInfo = {
+                    infohash: torrentDetails.infohash,
+                    name: torrentDetails.name,
+                    trackers: torrentDetails.trackers,
+                    peers: torrent.peers,
+                    seeds: torrent.seeds,
+                    provider: torrent.provider
+                };
+
+                objs.push(torrentInfo);
+            }
+
+            return objs;
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+
+// getInfohashesForTop100();
+getTorrentsForMoviesToTrack();
