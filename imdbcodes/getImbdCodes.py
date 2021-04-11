cd539310335184050ca4efb5941069b9d08dc419
CajoAlbuquerque <carlosjo.da@gmail.com>
2021-04-11 12:06:57 +0100

    IMDB codes; Usheru dataset

---
+++ imdbcodes/getImbdCodes.py
@@ -0,0 +1,19 @@
+import json
+import requests
+import re as regex
+
+def getIMDBcode(url):
+    response = requests.get(url)
+    result = regex.findall('www.imdb.com/title/\w*', response.text)
+    if len(result) > 0:
+        return result[0][19:]
+    return 'NA'
+
+
+with open('topTPB.json') as json_file:
+    data = json.load(json_file)
+    for movie in data:
+        movie['imdb'] = getIMDBcode(movie['id'])
+
+datafile = open('modifiedTPB.json', 'w')
+datafile.write(json.dumps(data))
